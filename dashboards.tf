36ff3c59d2d63cc5d61dbcaac208fa78f921a63f
edisoncastro01 <44499842+edisoncastro01@users.noreply.github.com>
2021-01-28 08:41:10 -0500

    Add files via upload

---
+++ dashboards.tf
@@ -0,0 +1,104 @@
+data "newrelic_entity" "pathpoint-trading-grid2" {
+  name = var.app_name
+  type = "APPLICATION"
+  domain = "APM"
+}
+resource "newrelic_dashboard" "pathpoint-trading-grid2" {
+  title             = var.dashboard_name
+  grid_column_count = 3
+
+  widget {
+    title         = "Links to APM Dashboard"
+    width         = 1
+    height        = 1
+    row           = 1
+    column        = 1
+    source        = <<-EOF
+      "[Go to APM Dashboard](https://one.newrelic.com/redirect/entity/${var.entity_id})"
+    EOF
+    visualization = "markdown"
+  }
+
+  widget {
+    title         = "Statistics"
+    nrql          = <<-EOF
+      SELECT 
+        count(*),max(duration) as max,min(duration) as min,average(duration) as average,median(duration),percentage(count(*), WHERE error is true) as percentage_error 
+        FROM Transaction 
+        WHERE appName='${var.app_name}' SINCE 30 minutes ago
+    EOF
+    visualization = "attribute_sheet"
+    width         = 1
+    height        = 1
+    row           = 1
+    column        = 2
+  }
+
+  widget {
+    title         = "Use"
+    nrql          = <<-EOF
+      SELECT 
+        count(*) as count 
+      FROM Transaction 
+      WHERE appName='${var.app_name}' 
+      TIMESERIES auto 
+      SINCE 30 minutes ago
+    EOF
+    visualization = "faceted_area_chart"
+    width         = 1
+    height        = 1
+    row           = 1
+    column        = 3
+  }
+
+  widget {
+    title         = "Response Time"
+    nrql          = <<-EOF
+      SELECT 
+        average(duration),median(duration),max(duration),min(duration)  
+      FROM Transaction 
+      WHERE appName='${var.app_name}' 
+      TIMESERIES AUTO 
+      SINCE 30 minutes ago
+    EOF
+    visualization = "line_chart"
+    width         = 1
+    height        = 1
+    row           = 2
+    column        = 1
+  }
+
+  widget {
+    title         = "Use by Agent"
+    nrql          = <<-EOF
+      SELECT 
+        count(*) 
+      FROM Transaction 
+      WHERE appName='${var.app_name}' 
+      TIMESERIES AUTO 
+      FACET request.headers.userAgent 
+      SINCE 30 minutes ago
+    EOF
+    visualization = "faceted_area_chart"
+    width         = 1
+    height        = 1
+    row           = 2
+    column        = 2
+  }
+
+  widget {
+    title         = "Use by Host"
+    nrql          = <<-EOF
+      SELECT 
+        count(*) FROM Transaction 
+      WHERE appName='${var.app_name}' 
+      FACET request.headers.host  
+      SINCE 30 minutes ago
+    EOF
+    visualization = "facet_bar_chart"
+    width         = 1
+    height        = 1
+    row           = 2
+    column        = 3
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "NEWRELIC_ACCOUNT_ID" {
+  type    = number
+}
+variable "NEWRELIC_API_KEY" {
+  type    = string
+}
+variable "NEWRELIC_REGION" {
+  type    = string
+}
+variable "dashboard_name" {
+  type    = string
+}
+variable "app_name" {
+  type    = string
+}
+variable "entity_id" {
+  type    = string
+}
